a7de83c69cf603dd996bfc85815415373e6ba731
leeyoungseung <rudnfskf2@gmail.com>
2022-07-13 00:02:28 +0900

    [04] Basic-Board : Read

--- src/main/java/com/sb/template/controller/BoardController.java
+++ src/main/java/com/sb/template/controller/BoardController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -46,6 +47,15 @@ public class BoardController {
 		boardNo = boardService.createBoard(form.toEntity()).getBoardNo();
 
 		return "redirect:/board/list";
+	}
+
+
+	@RequestMapping(method = RequestMethod.GET, path = "read/{boardNo}")
+	public String viewBoardOne(@PathVariable int boardNo, Model model) {
+
+        model.addAttribute("board", boardService.getBoardOne(boardNo));
+
+		return "board/read";
 	}
 
 }
--- src/main/java/com/sb/template/service/BoardService.java
+++ src/main/java/com/sb/template/service/BoardService.java
@@ -1,6 +1,7 @@
 package com.sb.template.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -25,5 +26,16 @@ public class BoardService {
 	public Board createBoard(Board board) {
 
 		return boardRepository.save(board);
+	}
+
+	public Board getBoardOne(int boardNo) {
+
+		Optional<Board> board = boardRepository.findById(boardNo);
+
+		if (board.isEmpty()) {
+			return null;
+		}
+
+		return board.get();
 	}
 }
